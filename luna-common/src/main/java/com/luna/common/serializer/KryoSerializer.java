a26725a6525bf149095b0d85754d558b46abed6c
moon <liuyc@fsmeeting.com>
2016-12-14 18:09:30 +0800

    ssss

--- luna-common/src/main/java/com/luna/common/serializer/KryoSerializer.java
+++ luna-common/src/main/java/com/luna/common/serializer/KryoSerializer.java
@@ -1,12 +1,27 @@
 package com.luna.common.serializer;
 
+import java.io.ByteArrayInputStream;
+
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.SerializationException;
 
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.serializers.JavaSerializer;
+//http://nettm.blog.51cto.com/4841905/1702453
 public class KryoSerializer implements RedisSerializer<Object> {
 
 	@Override
 	public byte[] serialize(Object t) throws SerializationException {
+
+		Kryo kryo = new Kryo();
+		kryo.setReferences(false);
+		kryo.register(clazz, new JavaSerializer());
+
+		ByteArrayInputStream bais = new ByteArrayInputStream(new Base64().decode(obj));
+		Input input = new Input(bais);
+		return (T) kryo.readClassAndObject(input);
 		return null;
 	}
 
